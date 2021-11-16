d02be3272cac7b9d13c188d86eddc0d27f59c4bb
wlgur123 <ahngod2718@gmail.com>
2021-11-16 22:29:14 +0900

    add getopt.sh file

--- getopt.sh
+++ getopt.sh
@@ -1,3 +1,12 @@
 #i/bin/bash
 #getopt command
 
+while getopt -o a:b: -l help:  
+do
+	echo "opt=$opt, OPTARG=$OPTARG"
+	case $opt in
+		--help)
+			echo "help is show more information about getopt command";;
+	esac
+
+done
